ba1f0e113af86610c87968a421fd6be1f1ffc667
Punit Inani <80308335+puni9869@users.noreply.github.com>
2021-09-17 18:21:43 +0530

    Server side rendering script

---
+++ tengine.js
@@ -0,0 +1,28 @@
+var TemplateEngine =(html, options)=>{
+	var re = /{{#(.+?)#}}/g, 
+		reExp = /(^( )?(const|let|var|if|for|else|switch|case|break|{|}|;))(.*)?/g, 
+		code = 'with(obj) { var r=[];\n', 
+		cursor = 0, 
+		result,
+	    	match;
+	var add = (line, js)=>{
+		js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+			(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+		return add;
+	}
+    while (match = re.exec(html)) {
+        console.log(match);
+		add(html.slice(cursor, match.index))(match[1], true);
+		cursor = match.index + match[0].length;
+	}
+	add(html.substr(cursor, html.length - cursor));
+    code = (code + 'return r.join(""); }').replace(/[\r\t\n]/g, ' ');
+    console.log(code);
+    try {
+        result = new Function('obj', code).apply(options, [options]);
+    }
+    catch (err) {
+        console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n");
+    }
+	return result;
+}
